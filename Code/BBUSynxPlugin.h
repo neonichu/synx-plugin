5f09c541d02c14721d50def3f32a1517ea2027d3
Boris Bügling <boris@icculus.org>
2014-06-23 00:48:08 +0200

    Implemented basic functionality.

--- Code/BBUSynxPlugin.h
+++ Code/BBUSynxPlugin.h
@@ -1,6 +1,6 @@
 //
 //  BBUsynx-plugin.h
-//  BBUsynx-plugin
+//  synx-plugin
 //
 //  Created by Boris Bügling on 22/06/14.
 //  Copyright (c) 2014 Boris Bügling. All rights reserved.
--- Code/BBUSynxPlugin.m
+++ Code/BBUSynxPlugin.m
@@ -1,10 +1,13 @@
 //
 //  BBUsynx-plugin.m
-//  BBUsynx-plugin
+//  synx-plugin
 //
 //  Created by Boris Bügling on 22/06/14.
 //    Copyright (c) 2014 Boris Bügling. All rights reserved.
 //
+
+#import <objc/runtime.h>
+#import <Aspects/Aspects.h>
 
 #import "BBUSynxPlugin.h"
 
@@ -35,6 +38,8 @@ static BBUSynxPlugin *sharedPlugin;
 {
     if (self = [super init]) {
         self.bundle = plugin;
+
+        [self swizzleOpeningWorkspaceDocument];
     }
     return self;
 }
@@ -42,6 +47,78 @@ static BBUSynxPlugin *sharedPlugin;
 - (void)dealloc
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (id)logOutputToPipe:(NSPipe*)pipe
+{
+    [[pipe fileHandleForReading] waitForDataInBackgroundAndNotify];
+
+    return [[NSNotificationCenter defaultCenter] addObserverForName:NSFileHandleDataAvailableNotification object:[pipe fileHandleForReading] queue:nil usingBlock:^(NSNotification*notification) {
+        NSData *output = [[pipe fileHandleForReading] availableData];
+        NSString *outStr = [[NSString alloc] initWithData:output encoding:NSUTF8StringEncoding];
+
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            NSLog(@"synx-plugin: %@", outStr);
+        });
+
+        [[pipe fileHandleForReading] waitForDataInBackgroundAndNotify];
+    }];
+}
+
+- (void)runTask:(NSTask*)task
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        task.standardError = [NSPipe new];
+        task.standardOutput = [NSPipe new];
+
+        id errorObserver = [self logOutputToPipe:task.standardError];
+        id outputObserver = [self logOutputToPipe:task.standardOutput];
+
+        [task launch];
+        [task waitUntilExit];
+        
+        [[NSNotificationCenter defaultCenter] removeObserver:errorObserver];
+        [[NSNotificationCenter defaultCenter] removeObserver:outputObserver];
+    });
+}
+
+- (void)swizzleOpeningWorkspaceDocument
+{
+    NSError* error = nil;
+    [objc_getClass("IDEWorkspaceDocument") aspect_hookSelector:@selector(readFromURL:ofType:error:) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> info, NSURL* url, id type, NSError** error) {
+        NSString* path = [url path];
+        NSString* project = nil;
+
+        if ([path.lastPathComponent isEqualToString:@"project.xcworkspace"]) {
+            project = [[path stringByDeletingLastPathComponent] lastPathComponent];
+            path = [[path stringByDeletingLastPathComponent] stringByDeletingLastPathComponent];
+        } else {
+            path = [path stringByDeletingLastPathComponent];
+
+            for (NSString* file in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path
+                                                                                       error:nil]) {
+                if ([file.pathExtension isEqualToString:@"xcodeproj"]) {
+                    project = file;
+                    break;
+                }
+            }
+        }
+
+        if (!path || !project) {
+            return;
+        }
+
+        NSTask* synxTask = [NSTask new];
+        [synxTask setArguments:@[ project ]];
+        [synxTask setCurrentDirectoryPath:path];
+        [synxTask setLaunchPath:@"/usr/bin/synx"];
+
+        [self runTask:synxTask];
+    } error:&error];
+
+    if (error) {
+        NSLog(@"Could not initialize synx-plugin: %@", error);
+    }
 }
 
 @end
